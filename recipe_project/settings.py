7e83fbe73931f4a23b5bd2f9ec84ab08c608c879
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-08-24 11:15:22 +0200

    Media Settings 2

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-   BASE_DIR / 'static'
+   os.path.join(BASE_DIR, "static"),
 ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
