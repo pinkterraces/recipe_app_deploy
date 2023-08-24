94a071b9826fce10d79aa05a653a6f063fccf4c5
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-08-24 11:31:41 +0200

    Media Settings 4 putting back original

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -119,16 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-   os.path.join(BASE_DIR, "static"),
-]
 
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
